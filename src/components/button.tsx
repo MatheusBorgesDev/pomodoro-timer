10ddf2c371a7508cdb436dcb4c51e1e88c70ffb0
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-05-06 20:49:20 -0300

    feat: :sparkles: add routes with react router dom

--- src/components/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonRender, buttonVariant } from "./button.styles";
-
-interface ButtonProps {
-  variant?: buttonVariant;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-  return <ButtonRender variant={variant}>Enviar</ButtonRender>;
-}
