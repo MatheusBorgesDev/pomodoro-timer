bebc30271dbb48836f4eabc528d047acf25fdd9e
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-05-18 08:09:22 -0300

    chore: :recycle: add context to form component

--- src/pages/home/components/newCycleForm/index.tsx
+++ src/pages/home/components/newCycleForm/index.tsx
@@ -1,27 +1,13 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { useContext } from "react";
+import { CyclesContext } from "../..";
+
+import { useFormContext } from "react-hook-form";
 
 import { FormRender, MinutesCountdownInput, TaskInput } from "./styles";
 
-const newCycleFormSchema = z.object({
-  task: z.string().min(1, "Informe a tarefa"),
-  minutesAmount: z
-    .number()
-    .min(5, { message: "O ciclo precisa ser de no mínimo 5 minutos" })
-    .max(60, { message: "O ciclo precisa ser de no máximo 60 minutos" }),
-});
-
-type newCycleFormData = z.infer<typeof newCycleFormSchema>;
-
 export function NewCycleForm() {
-  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
-    resolver: zodResolver(newCycleFormSchema),
-    defaultValues: {
-      task: "",
-      minutesAmount: 0,
-    },
-  });
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
 
   return (
     <FormRender>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,8 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
+
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 import { HandPalm, Play } from "@phosphor-icons/react";
 
@@ -23,14 +27,34 @@ interface Cycle {
 interface CyclesContextType {
   activeCycle: Cycle | undefined;
   activeCycleId: string | null;
-  markCurrentCycleAsFinished: () => void
+  markCurrentCycleAsFinished: () => void;
 }
 
 export const CyclesContext = createContext({} as CyclesContextType);
 
+const newCycleFormSchema = z.object({
+  task: z.string().min(1, "Informe a tarefa"),
+  minutesAmount: z
+    .number()
+    .min(5, { message: "O ciclo precisa ser de no mínimo 5 minutos" })
+    .max(60, { message: "O ciclo precisa ser de no máximo 60 minutos" }),
+});
+
+type newCycleFormData = z.infer<typeof newCycleFormSchema>;
+
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
+  const newCycleForm = useForm<newCycleFormData>({
+    resolver: zodResolver(newCycleFormSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const { handleSubmit, watch, reset } = newCycleForm;
 
   const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId);
 
@@ -53,7 +77,7 @@ export function Home() {
     reset();
   }
 
-  function markCurrentCycleAsFinished(){
+  function markCurrentCycleAsFinished() {
     setCycles((state) =>
       state.map((cycle) => {
         if (cycle.id == activeCycleId) {
@@ -62,6 +86,7 @@ export function Home() {
           return cycle;
         }
       })
+    );
   }
 
   function handleInterruptCycle() {
@@ -81,8 +106,13 @@ export function Home() {
   return (
     <HomeRender>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}>
-          <NewCycleForm />
+        <CyclesContext.Provider
+          value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}
+        >
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
+
           <Countdown />
         </CyclesContext.Provider>
 
